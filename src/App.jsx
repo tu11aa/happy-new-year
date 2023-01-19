e23a50de5e723abc97a943db78404036fc8a4b2c
tu11aa <thanhtuhr01@gmail.com>
2023-01-19 23:52:23 +0700

    latest version, bot deploy on gh page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
-import { wish } from "./data.js";
+import {
+  toWish,
+  wish,
+  wishsRelatives,
+  wishsParents,
+  wishsCousin,
+} from "./data.js";
 
 function App() {
+  const accessTokens = ["friends", "family", "TET", "tet", "Ngiu", "@Quin<3"];
+
   const [text, setText] = useState("");
+  const [type, setType] = useState("");
+  const [typeClass, setTypeClass] = useState("code");
 
   const handleDate = (e) => {
     const value = e.target.value;
@@ -10,10 +20,69 @@ function App() {
     else setText("");
   };
 
-  return (
+  const handleRelative = (e) => {
+    const value = e.target.value;
+    setText(toWish(value, wishsRelatives));
+  };
+
+  const handleFamily = (e) => {
+    const value = e.target.value;
+    if (value === "Dung")
+      setText(
+        "Dung năm nay bớt trẻ trâu lại, bớt ăn lại đi chứ béo lắm rồi đó, rồi sống tiết kiệm lại tí thì a2 lâu lâu mới cho tí tiền sài, không thì mơ đi :))"
+      );
+    else setText(toWish(value, wishsParents));
+  };
+
+  const handleAccessToken = (e) => {
+    setType(e.target.value);
+    if (accessTokens.find((accessToken) => accessToken === e.target.value))
+      setTypeClass("none");
+  };
+
+  return typeClass === "code" ? (
+    <input
+      className="code"
+      type="text"
+      value={type}
+      onChange={handleAccessToken}
+      placeholder="Access token"
+    />
+  ) : (
     <>
-      <input className="date" type="date" onChange={handleDate} />
-      <p className="text">{text}</p>
+      {type === accessTokens[0] && (
+        <>
+          <input className="date z-index" type="date" onChange={handleDate} />
+          <p className="text">{text}</p>
+        </>
+      )}
+      {type === accessTokens[1] && (
+        <>
+          <select className="options z-index" onChange={handleFamily}>
+            <option value="Cha">Cha</option>
+            <option value="Mẹ">Mẹ</option>
+            <option value="Dung">Dung</option>
+          </select>
+          <p className="text">{text}</p>
+        </>
+      )}
+      {type === accessTokens[2] && (
+        <>
+          <select className="options z-index" onChange={handleRelative}>
+            <option value="Gia đình Cậu 2 ">Gia đình Cậu 2</option>
+            <option value="Gia đình Mẹ 3">Gia đình Mẹ 3</option>
+            <option value="Gia đình Mẹ 4">Gia đình Mẹ 4</option>
+            <option value="Gia đình Mẹ 5">Gia đình Mẹ 5</option>
+            <option value="Gia đình Mẹ Út">Gia đình Mẹ Út</option>
+          </select>
+          <p className="text">{text}</p>
+        </>
+      )}
+      {type === accessTokens[3] && (
+        <p className="text">{toWish("", wishsCousin)}</p>
+      )}
+      {type === accessTokens[4] && <p className="text"></p>}
+      {type === accessTokens[5] && <p className="text"></p>}
       <div className="feliz">Happy new year</div>
       <div className="ano_novo">
         <span>202</span>
--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ const birthday = {
   Quynh: "2000-10-17",
   Phuong: "2002-07-05",
   Nam: "2002-08-13",
-  Dai: "2002-00-25",
+  Dai: "2002-03-25",
   Diep: "2002-09-30",
   Quang: "2002-03-07",
   Thuan: "2002-06-02",
@@ -12,17 +12,17 @@ const birthday = {
   Mai: "2003-08-11",
 };
 
-const wishsParents = [
-  "Nhân dịp năm mới, con muốn gửi đến bố mẹ lời chúc ý nghĩa và tốt đẹp nhất. Chúc bố mẹ luôn mạnh khỏe, tươi trẻ và có một năm tràn ngập niềm vui, lúc nào gia đình ta cũng quây quần, tràn ngập tiếng cười.",
-  "Gia đình mình thật hạnh phúc và vui vẻ, đặc biệt là khi có ông bà bên cạnh bố mẹ và cháu. Tết này cháu kính chúc ông bà luôn mạnh khỏe, sống thật lâu bên gia đình của mình.",
-  "Một năm mới mang đến cho chúng ta cơ hội để biến ước mơ thành hiện thực, giúp cuộc sống tốt hơn. Con mong năm mới cũng mang đến cho bố mẹ những điều tốt đẹp nhất. Chúc mừng năm mới.",
-  "Bố mẹ chính là người sinh thành, dưỡng dục, là tấm gương sáng để con noi theo. Hy vọng năm mới 2023 sẽ mang đến cho bố mẹ niềm vui, hạnh phúc. Chúc mừng năm mới, con yêu bố mẹ.",
-  "Một năm mới mang đến cho ta cơ hội để biến ước mơ trở thành hiện thực, giúp cuộc sống trở nên tốt hơn mỗi ngày. Con mong năm mới 2023 cũng mang đến cho bố mẹ những điều tốt đẹp nhất. Chúc mừng năm mới nhà mình!",
+export const wishsParents = [
+  "Nhân dịp năm mới, con muốn gửi đến @name lời chúc ý nghĩa và tốt đẹp nhất. Chúc @name luôn mạnh khỏe, tươi trẻ và có một năm tràn ngập niềm vui, lúc nào gia đình ta cũng quây quần, tràn ngập tiếng cười.",
+  "Gia đình mình thật hạnh phúc và vui vẻ, đặc biệt là khi có ông bà bên cạnh @name và cháu. Tết này cháu kính chúc ông bà luôn mạnh khỏe, sống thật lâu bên gia đình của mình.",
+  "Một năm mới mang đến cho chúng ta cơ hội để biến ước mơ thành hiện thực, giúp cuộc sống tốt hơn. Con mong năm mới cũng mang đến cho @name những điều tốt đẹp nhất. Chúc mừng năm mới.",
+  "@name chính là người sinh thành, dưỡng dục, là tấm gương sáng để con noi theo. Hy vọng năm mới 2023 sẽ mang đến cho @name niềm vui, hạnh phúc. Chúc mừng năm mới, con yêu @name.",
+  "Một năm mới mang đến cho ta cơ hội để biến ước mơ trở thành hiện thực, giúp cuộc sống trở nên tốt hơn mỗi ngày. Con mong năm mới 2023 cũng mang đến cho @name những điều tốt đẹp nhất. Chúc mừng năm mới nhà mình!",
   "Năm mới đến, con không có ước nguyện gì ngoài việc mong cha mẹ sức khoẻ, bình an, luôn là động lực, chỗ dựa cho con cháu.",
-  "Lại một năm mới sắp đến, con cảm thấy mình thật may mắn khi vẫn có đủ đầy cả bố cả mẹ. Con cảm ơn bố mẹ đã luôn ở bên, chia sẻ, thấu hiểu và giúp đỡ con trong cuộc sống.",
+  "Lại một năm mới sắp đến, con cảm thấy mình thật may mắn khi vẫn có đủ đầy cả bố cả mẹ. Con cảm ơn @name đã luôn ở bên, chia sẻ, thấu hiểu và giúp đỡ con trong cuộc sống.",
 ];
 
-const wishsRelatives = [
+export const wishsRelatives = [
   "Nhân dịp năm mới, con muốn gửi đến @name lời chúc ý nghĩa và tốt đẹp nhất. Chúc @name luôn mạnh khỏe, tươi trẻ và có một năm tràn ngập niềm vui, lúc nào gia đình cũng quây quần, tràn ngập tiếng cười.",
   // "Gia đình mình thật hạnh phúc và vui vẻ, đặc biệt là khi có ông bà bên cạnh @name và cháu. Tết này cháu kính chúc ông bà luôn mạnh khỏe, sống thật lâu bên gia đình của mình.",
   "Một năm mới mang đến cho chúng ta cơ hội để biến ước mơ thành hiện thực, giúp cuộc sống tốt hơn. Con mong năm mới cũng mang đến cho @name những điều tốt đẹp nhất. Chúc mừng năm mới.",
@@ -32,8 +32,8 @@ const wishsRelatives = [
   // "Lại một năm mới sắp đến, con cảm thấy mình thật may mắn khi vẫn có đủ đầy cả bố cả mẹ. Con cảm ơn @name đã luôn ở bên, chia sẻ, thấu hiểu và giúp đỡ con trong cuộc sống.",
 ];
 
-const wishsCousin = [
-  "Chúc mừng năm mới! Chúc toàn thể gia đình bạn tôi vạn sự như ý, tỉ sự như mơ, triệu triệu bất ngờ, không chờ cũng đến!",
+export const wishsCousin = [
+  // "Chúc mừng năm mới! Chúc toàn thể gia đình bạn tôi vạn sự như ý, tỉ sự như mơ, triệu triệu bất ngờ, không chờ cũng đến!",
   "Chúc bạn 12 tháng phú quý, 365 ngày phát tài, 8760 giờ sung túc, 525600 phút thành công, 31536000 giây vạn sự như ý.",
   "Năm mới chúc bạn sức khỏe nhiều. Bạc tiền rủng rỉnh thoải mái tiêu. Gia đình hạnh phúc bè bạn quý. Thanh thản vui chơi khỏi nghĩ nhiều.",
   "Cung chúc tân niên, sức khỏe vô biên, thành công liên miên, hạnh phúc triền miên, túi luôn đầy tiền, sung sướng như tiên.",
@@ -44,13 +44,17 @@ const wishsCousin = [
   "Năm mới 2023, chúc anh chị em tiền vào cửa trước, tiền ra cửa sau, hai cái đâm nhau, chui vào két sắt, phần thì mua đất, phần thì mua vàng, lang thang du lịch.",
 ];
 
-const toWish = (name, wishs) => {
-  return wishs[Math.floor(Math.random() * wishs.length)].replace("@name", name);
+export const toWish = (name, wishs) => {
+  return wishs[Math.floor(Math.random() * wishs.length)].replaceAll(
+    "@name",
+    name
+  );
 };
 
 export const wish = {
   "2001-12-24": "Người đặt biệt, nên phải chúc theo kiểu đặt biệt nhoeeeeee <3",
-  "2000-10-17": "Chẹp, nào gặp nói cho nghe :)))",
+  "2000-10-17":
+    "Á à bà chị tui dễ thương vui tánh :v. Nhưng mà bị cái tật nhây quá nha bà dà :))). Năm mới chúc chị ",
   "2002-08-13":
     "Nam chơi game hay cọc nhưng được cái ở ngoài vô tri vl :)), tuy nhiên tính tình rất tốt nha, quan tâm bạn bè và rất nhiệt tình. Chúc Nam với Mai Phương luôn dui dẻ, công việc sẽ tiến tới bước cao hơn như quản lý chẳng hạn, sao đó bao chúng toy mấy chầu ăn uốn phê pha :)))))",
   "2002-03-07":
@@ -67,4 +71,6 @@ export const wish = {
     "Nói chuyện rất vui, tính cũng vui, toàn bị bọn t chửi nhưng mà mở miệng ra cười hì hì thôi. Nhưng mà chơi chung với m vui thật sự, không dõi hay kiểu mè nheo như mấy tụi con gái bánh bèo. Dạo này ít chơi chung lại gòi, nhưng bạn vẫn mãi keo trong lòng toy nháaa. Năm mới vui vẻ, đạt được thành tựu mình mong muốn nhaaaaa",
   "2001-12-22":
     "Mặc dù hay chửi m nhưng không có ý gì đâu nha bàaaa. Thật sự toy xem bà là 1 người bạn đó, chơi chung vui nha. Dạo này học khác nhau nên cũng ít gặp, nhưng bạn vẫn chiếm 1 slot trong friend list của toy nhá. Chúc năm mới vui vẻ, nhiều sức khỏe để chống chọi với deadline nha.",
+  "2002-03-25":
+    "Tuy là mới biết nhau nhưng mà đc cái quăng miếng là bắt quăng miếng là bắt lẹ lắm:))). Bị cái Đại hay bận chứ không là nguyên team chơi chung nhiều hơn rồi :v. Chúc năm mới vui vẻ, sống khỏe nhá, và đặt biệt ít bận hơn để đi chơi với bọn toy :v",
 };
